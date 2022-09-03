8f618d60052240226f5ec933529c429550791877
lexangross51 <lexangross51@gmail.com>
2022-09-03 23:39:03 +0700

    Добавил силу тока для каждого источника

--- problem_1/Parameters.cs
+++ problem_1/Parameters.cs
@@ -6,7 +6,7 @@ public readonly record struct Point3D(double X, double Y, double Z)
         Math.Sqrt((b.X - a.X) * (b.X - a.X) + (b.Y - a.Y) * (b.Y - a.Y) + (b.Z - a.Z) * (b.Z - a.Z));
 }
 
-public readonly record struct PowerSource(Point3D A, Point3D B);
+public readonly record struct PowerSource(Point3D A, Point3D B, double RealCurrent, double PrimaryCurrent);
 
 public readonly record struct PowerReceiver(Point3D M, Point3D N);
 
@@ -15,6 +15,4 @@ public class Parameters
     public PowerSource[] PowerSources { get; init; } = Array.Empty<PowerSource>();
     public PowerReceiver[] PowerReceivers { get; init; } = Array.Empty<PowerReceiver>();
     public double Sigma { get; init; }
-    public double RealCurrent { get; init; }
-    public double PrimaryCurrent { get; init; }
 }
