91191d4c7408957765b0ac58ab39c432184ec84f
shishnk <akorund@mail.ru>
2022-09-03 14:54:16 +0700

    Добавил классы, представляющие плотную матрицу и вектор

---
+++ problem_1/Matrix.cs
@@ -0,0 +1,16 @@
+﻿namespace problem_1;
+
+public class Matrix<T> where T: INumber<T>
+{
+    private readonly T[,] _storage;
+    public int Size { get; }
+
+    public T this[int i, int j]
+    {
+        get => _storage[i, j];
+        set => _storage[i, j] = value;
+    }
+
+    public Matrix(int size)
+        => (Size, _storage) = (size, new T[size, size]);
+}
