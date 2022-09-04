c99da8d17ec90a4aed7008fdeceda6db67eb6dee
lexangross51 <lexangross51@gmail.com>
2022-09-04 13:57:14 +0700

    Добавил регуляризацию. Немного поправлен расчет вектора.

--- problem_1/ElectroExploration.cs
+++ problem_1/ElectroExploration.cs
@@ -29,14 +29,21 @@ public class ElectroExploration
     private Matrix<double> _realPotentials = default!;
     private Matrix<double> _potentialsDiffs = default!;
     private Vector<double> _vector = default!;
+    private Vector<double> _currents = default!;
 
     private void Init()
     {
         _matrix = new(_parameters.PowerSources.Length);
         _vector = new(_parameters.PowerSources.Length);
+        _currents = new(_parameters.PowerSources.Length);
         _primaryPotentials = new(_parameters.PowerSources.Length, _parameters.PowerReceivers.Length);
         _realPotentials = new(_parameters.PowerSources.Length, _parameters.PowerReceivers.Length);
         _potentialsDiffs = new(_parameters.PowerSources.Length, _parameters.PowerReceivers.Length);
+
+        for (int i = 0; i < _currents.Size; i++)
+        {
+            _currents[i] = _parameters.PowerSources[i].PrimaryCurrent;
+        }
     }
 
     public void Compute()
@@ -48,25 +55,14 @@ public class ElectroExploration
         _solver.SetMatrix(_matrix);
         _solver.SetVector(_vector);
 
-        for (int i = 0; i < _matrix.Rows; i++)
-        {
-            for (int j = 0; j < _matrix.Columns; j++)
-            {
-                Console.Write($"{_matrix[i, j]}   ");
-            }
-
-            Console.WriteLine();
-        }
-
-        for (int i = 0; i < _vector.Size; i++)
-        {
-            Console.WriteLine($"{_vector[i]}");
-        }
-
-
         _solver.Compute();
 
-        Array.ForEach(_solver.Solution!.Value.ToArray(), Console.WriteLine);
+        for (int i = 0; i < _currents.Size; i++)
+        {
+            _currents[i] += _solver.Solution!.Value[i];
+        }
+
+        Array.ForEach(_currents.ToImmutableArray().ToArray(), Console.WriteLine);
     }
 
     private void DataGeneration()
@@ -76,11 +72,11 @@ public class ElectroExploration
             for (int j = 0; j < _parameters.PowerReceivers.Length; j++)
             {
                 _potentialsDiffs[i, j] =
-                    1.0 / (2.0 * Math.PI * _parameters.Sigma) *
-                    (1.0 / Point3D.Distance(_parameters.PowerSources[i].B, _parameters.PowerReceivers[j].M) -
-                     1.0 / Point3D.Distance(_parameters.PowerSources[i].A, _parameters.PowerReceivers[j].M) -
-                     1.0 / Point3D.Distance(_parameters.PowerSources[i].B, _parameters.PowerReceivers[j].N) +
-                     1.0 / Point3D.Distance(_parameters.PowerSources[i].A, _parameters.PowerReceivers[j].N));
+                    1.0 / (2.0 * Math.PI * _parameters.Sigma) * (
+                    1.0 / Point3D.Distance(_parameters.PowerSources[i].B, _parameters.PowerReceivers[j].M) -
+                    1.0 / Point3D.Distance(_parameters.PowerSources[i].A, _parameters.PowerReceivers[j].M) -
+                    1.0 / Point3D.Distance(_parameters.PowerSources[i].B, _parameters.PowerReceivers[j].N) +
+                    1.0 / Point3D.Distance(_parameters.PowerSources[i].A, _parameters.PowerReceivers[j].N));
 
                 _realPotentials[i, j] = _potentialsDiffs[i, j] * _parameters.PowerSources[i].RealCurrent;
 
@@ -91,22 +87,37 @@ public class ElectroExploration
 
     private void AssemblySystem()
     {
-        for (int q = 0; q < _parameters.PowerSources.Length; q++)
+        for (int i = 0; i < _parameters.PowerSources.Length; i++)
         {
-            for (int s = 0; s < _parameters.PowerSources.Length; s++)
+            for (int q = 0; q < _parameters.PowerSources.Length; q++)
             {
-                for (int i = 0; i < _parameters.PowerSources.Length; i++)
+                for (int s = 0; s < _parameters.PowerSources.Length; s++)
                 {
                     for (int j = 0; j < _parameters.PowerReceivers.Length; j++)
                     {
                         double w = 1.0 / _realPotentials[i, j];
 
                         _matrix[q, s] += w * w * _potentialsDiffs[i, j] * _potentialsDiffs[i, j];
-
-                        _vector[q] -= w * w * _potentialsDiffs[i, j] *
-                                      (_primaryPotentials[i, j] - _realPotentials[i, j]);
                     }
                 }
+
+                for (int j = 0; j < _parameters.PowerReceivers.Length; j++)
+                {
+                    double w = 1.0 / _realPotentials[i, j];
+
+                    _vector[q] -= w * w * _potentialsDiffs[i, j] * (_primaryPotentials[i, j] - _realPotentials[i, j]);
+                }
+            }
+        }
+
+        // Регуляризация
+        for (int i = 0; i < _matrix.Rows; i++)
+        {
+            _matrix[i, i] += 1e-10;
+
+            for (int j = 0; j < _parameters.PowerSources.Length; j++)
+            {
+                _vector[i] -= 1e-10 * (_parameters.PowerSources[j].PrimaryCurrent - _parameters.PowerSources[j].RealCurrent);
             }
         }
     }
