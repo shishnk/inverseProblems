a7ce2efa155cd024aafee99ef94e17dbbe423ecd
lexangross51 <lexangross51@gmail.com>
2022-09-04 22:48:53 +0700

    Добавил метод для копирования матрицы

--- problem_1/Matrix.cs
+++ problem_1/Matrix.cs
@@ -1,6 +1,6 @@
 ﻿namespace problem_1;
 
-public class Matrix<T> where T: INumber<T>
+public class Matrix<T> where T : INumber<T>
 {
     private readonly T[][] _storage;
     public int Rows { get; }
@@ -16,13 +16,28 @@ public class Matrix<T> where T: INumber<T>
     {
         Rows = size;
         Columns = size;
-        _storage = new T[size].Select(_ => new T[size]).ToArray();;
+        _storage = new T[size].Select(_ => new T[size]).ToArray(); ;
     }
 
     public Matrix(int rows, int columns)
     {
         Rows = rows;
         Columns = columns;
-        _storage = new T[rows].Select(_ => new T[columns]).ToArray();;
+        _storage = new T[rows].Select(_ => new T[columns]).ToArray(); ;
+    }
+
+    public static Matrix<T> Copy(Matrix<T> otherMatrix)
+    {
+        Matrix<T> newMatrix = new(otherMatrix.Rows, otherMatrix.Columns);
+
+        for (int i = 0; i < otherMatrix.Rows; i++)
+        {
+            for (int j = 0; j < otherMatrix.Columns; j++)
+            {
+                newMatrix[i, j] = otherMatrix[i, j];
+            }
+        }
+
+        return newMatrix;
     }
 }
