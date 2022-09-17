0612155f6ce1f80d15b4f1792255cc1bd73ad759
shishnk <akorund@mail.ru>
2022-09-18 00:56:16 +0700

    Будущий основной вычислительный класс

---
+++ problem_2/ElectroExploration.cs
@@ -0,0 +1,14 @@
+﻿namespace problem_2;
+
+public class ElectroExploration
+{
+    public class ElectroExplorationBuilder
+    {
+        private readonly ElectroExploration _electroExploration = new();
+
+        public static implicit operator ElectroExploration(ElectroExplorationBuilder builder)
+            => builder._electroExploration;
+    }
+
+    public static ElectroExplorationBuilder CreateBuilder() => new();
+}
