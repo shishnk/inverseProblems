a17d1943e56dbe03e2061757aa8d83b0e94dafc2
lexangross51 <lexangross51@gmail.com>
2022-09-22 11:06:34 +0700

    Интерфейсы

---
+++ problem_2/Interfaces/IBasis.cs
@@ -0,0 +1,8 @@
+﻿namespace problem_2;
+
+public interface IBasis
+{
+    public double Psi(int ifunc, Point2D point);
+
+    public double DPsi(int iFunc, int iVar, Point2D point);
+}
---
+++ problem_2/Interfaces/IBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace problem_2;
+
+public interface IMeshBuilder
+{
+    Point2D[] CreatePoints();
+    FiniteElement[] CreateElements();
+    double[] CreateMaterials();
+}
