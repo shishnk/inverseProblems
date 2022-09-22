9dd6e8775f5ea088e21e8532747bb81359e3ebdf
lexangross51 <lexangross51@gmail.com>
2022-09-22 11:07:19 +0700

    Модуль геометрии: включает структуру Point2D и Interval

---
+++ problem_2/Source/Geometry.cs
@@ -0,0 +1,19 @@
+﻿namespace problem_2;
+
+public readonly record struct Point2D(double R, double Z)
+{
+    public override string ToString() => $"R: {R}, Z: {Z}";
+
+    public static double Distance(Point2D a, Point2D b) =>
+        Math.Sqrt((b.R - a.R) * (b.R - a.R) + (b.Z - a.Z) * (b.Z - a.Z));
+}
+
+public readonly record struct Interval
+{
+    [JsonProperty("Left border")] public double LeftBorder { get; init; }
+    [JsonProperty("Right border")] public double RightBorder { get; init; }
+    [JsonIgnore] public double Length => Math.Abs(RightBorder - LeftBorder);
+
+    [JsonConstructor] public Interval(double leftBorder, double rightBorder) =>
+        (LeftBorder, RightBorder) = (leftBorder, rightBorder);
+}
