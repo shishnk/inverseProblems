e3188b2dc3877566f0afbf22bf60deddc5e25bc9
lexangross51 <lexangross51@gmail.com>
2022-09-22 11:08:50 +0700

    Класс для расчета прямой задачи методом конченых элементов

---
+++ problem_2/Source/FEM.cs
@@ -0,0 +1,19 @@
+﻿namespace problem_2;
+
+public class FEM
+{
+    private Mesh _mesh = default!;
+
+    public FEM(Mesh mesh) => _mesh = mesh;
+
+
+    private void BuildLocalMatrices(int iElem)
+    {
+
+    }
+
+    private void AssemblySLAE()
+    {
+
+    }
+}
