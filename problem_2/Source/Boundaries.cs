efe72cceac47e158e0968001a79a5fc1a12a86c6
lexangross51 <lexangross51@gmail.com>
2022-09-25 00:59:50 +0700

    Структуры для хранения информации об условиях Дирихле и Неймана

---
+++ problem_2/Source/Boundaries.cs
@@ -0,0 +1,5 @@
+﻿namespace problem_2;
+
+public readonly record struct DirichletBoundary(int Node, double Value);
+
+public readonly record struct NeumannBoundary(int Elem, int Local1, int Local2, double Tetta);
