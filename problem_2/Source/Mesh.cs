56aac3bfc858d486c5c67b59ee992233ff6da0e7
lexangross51 <lexangross51@gmail.com>
2022-09-25 01:01:28 +0700

    Добавлены краевые условия

--- problem_2/Source/Mesh.cs
+++ problem_2/Source/Mesh.cs
@@ -5,12 +5,22 @@ public class Mesh
     public ImmutableArray<Point2D> Points { get; }
     public ImmutableArray<FiniteElement> Elements { get; }
     [JsonIgnore] public ImmutableArray<double> AreaProperty { get; }
+    [JsonIgnore] public ImmutableArray<DirichletBoundary> Dirichlet { get; } 
+    [JsonIgnore] public ImmutableArray<NeumannBoundary> Neumann { get; } 
 
-    public Mesh(Point2D[] points, FiniteElement[] elements, double[] properties)
+    public Mesh(
+        Point2D[] points, 
+        FiniteElement[] elements, 
+        double[] properties,
+        DirichletBoundary[] dirichlet,
+        NeumannBoundary[] neumann
+    )
     {
         Points = points.ToImmutableArray();
         Elements = elements.ToImmutableArray();
         AreaProperty = properties.ToImmutableArray();
+        Dirichlet = dirichlet.ToImmutableArray();
+        Neumann = neumann.ToImmutableArray();
     }
 
     public void Save(string path)
