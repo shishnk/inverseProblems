c389493b7d6b9d36f4cd3212fd7926e85287438e
lexangross51 <lexangross51@gmail.com>
2022-09-25 01:03:25 +0700

    Клиентский код

--- problem_2/Program.cs
+++ problem_2/Program.cs
@@ -1,8 +1,11 @@
 ﻿using problem_2;
+using problem_2.Source;
 
 MeshParameters parameters = MeshParameters.ReadJson("Parameters.json");
 MeshGenerator meshGen = new(new MeshBuilder(parameters));
 var mesh = meshGen.CreateMesh();
 
 FEMBuilder femBuilder = new();
-var fem = femBuilder.SetMesh(mesh).SetBasis(new LinearBasis()).SetSolver(new LOS(100, 1e-14)).Build();
+var fem = femBuilder.SetMesh(mesh).SetBasis(new LinearBasis()).SetSolver(new LOS(100, 1e-14)).SetTest(new Test1()).Build();
+
+fem.Solve();
