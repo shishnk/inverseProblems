7b011478c6f5f369687e6460b91039d740369560
lexangross51 <lexangross51@gmail.com>
2022-09-24 16:37:59 +0700

    Настройки проекта

--- problem_2/Program.cs
+++ problem_2/Program.cs
@@ -1,1 +1,8 @@
-﻿// GridFactory gridFactory = new RegularGridFactory(GridParameters.ReadJson("gridParameters.json"));
+﻿using problem_2;
+
+MeshParameters parameters = MeshParameters.ReadJson("Parameters.json");
+MeshGenerator meshGen = new(new MeshBuilder(parameters));
+var mesh = meshGen.CreateMesh();
+
+FEMBuilder femBuilder = new();
+var fem = femBuilder.SetMesh(mesh).SetBasis(new LinearBasis()).SetSolver(new LOS(100, 1e-14)).Build();
