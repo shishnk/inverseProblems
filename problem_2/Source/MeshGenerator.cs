aae1c52fe38c95d5db5bcc215282aaf3e7b15e8a
lexangross51 <lexangross51@gmail.com>
2022-09-25 01:02:27 +0700

    Добавлены вызовы методов для формирования краевых условий

--- problem_2/Source/MeshGenerator.cs
+++ problem_2/Source/MeshGenerator.cs
@@ -4,10 +4,11 @@ public class MeshGenerator
 {
     private readonly IMeshBuilder _builder;
     public MeshGenerator(IMeshBuilder builder) => _builder = builder;
-
     public Mesh CreateMesh() => new Mesh(
         _builder.CreatePoints(),
         _builder.CreateElements(),
-        _builder.CreateMaterials()
+        _builder.CreateMaterials(),
+        _builder.CreateDirichlet(), 
+        _builder.CreateNeumann()
     );
 }
