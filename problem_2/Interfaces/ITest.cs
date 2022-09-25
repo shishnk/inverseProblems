3204acee228948956afd090cfe7aded83f856b22
lexangross51 <lexangross51@gmail.com>
2022-09-25 20:40:07 +0700

    Изменил структуру теста

--- problem_2/Interfaces/ITest.cs
+++ problem_2/Interfaces/ITest.cs
@@ -4,6 +4,4 @@ public interface ITest
 {
     double V(double x, double y);
     double J(double x, double y);
-    double ThetaUp(double x, double y);
-    double ThetaLeft(double x, double y);
 }
--- problem_2/Source/Test.cs
+++ problem_2/Source/Test.cs
@@ -4,6 +4,4 @@ public class Test1 : ITest
 {
     public double V(double r, double z) => z;
     public double J(double r, double z) => 0.0;
-    public double ThetaUp(double r, double z) => 0.0;
-    public double ThetaLeft(double r, double z) => 0.0;
 };
