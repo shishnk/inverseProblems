326d21bb0e9aedbc5cd6413247e857e766ac55a0
lexangross51 <lexangross51@gmail.com>
2022-09-25 21:16:49 +0700

    Клиентский код

--- problem_2/Program.cs
+++ problem_2/Program.cs
@@ -3,7 +3,10 @@ MeshGenerator meshGenerator = new(new MeshBuilder(meshParameters));
 var mesh = meshGenerator.CreateMesh();
 mesh.Save("Mesh.json");
 
+Func<double, double, double> field = (double r, double z) => z;
+Func<double, double, double> source = (double r, double z) => 0.0;
+
 FEMBuilder femBuilder = new();
-FEMBuilder.FEM fem = femBuilder.SetMesh(mesh).SetBasis(new LinearBasis()).SetSolver(new LOSLU(1000, 1e-13)).SetTest(new Test1());
+FEMBuilder.FEM fem = femBuilder.SetMesh(mesh).SetBasis(new LinearBasis()).SetSolver(new LOSLU(1000, 1e-13)).SetTest(source, field);
 
 Console.WriteLine($"Residual: {fem.Solve()}");
