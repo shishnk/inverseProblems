6c14a9402f575fc9688546d7515320abd7dbc46a
lexangross51 <lexangross51@gmail.com>
2022-09-25 19:15:53 +0700

    Добавлены расширения для массивов

--- problem_2/Source/Extensions.cs
+++ problem_2/Source/Extensions.cs
@@ -14,3 +14,24 @@ public static class EnumerableExtensions
         return Math.Sqrt(Convert.ToDouble(scalar));
     }
 }
+
+public static class ArrayHelper
+{
+    public static T[] Copy<T>(this T[] source, T[] destination)
+    {
+        for (int i = 0; i < source.Length; i++)
+        {
+            destination[i] = source[i];
+        }
+
+        return destination;
+    }
+
+    public static void Fill<T>(this T[] array, T value)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = value;
+        }
+    }
+}
