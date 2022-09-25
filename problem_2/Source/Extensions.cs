6f9013502845a7570be95f9db7730e407168875b
lexangross51 <lexangross51@gmail.com>
2022-09-25 14:06:46 +0700

    Расширение для IEnumerable, позволяющее считатать норму коллекции

---
+++ problem_2/Source/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace problem_2.Source;
+
+public static class EnumerableExtensions
+{
+    public static double Norm<T>(this IEnumerable<T> collection) where T : INumber<T>
+    {
+        T scalar = T.Zero;
+
+        foreach (var item in collection)
+        {
+            scalar += item * item;
+        }
+
+        return Math.Sqrt(Convert.ToDouble(scalar));
+    }
+}
