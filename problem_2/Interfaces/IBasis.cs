f31246c036425268a2a749f623230c7d18db5381
lexangross51 <lexangross51@gmail.com>
2022-09-25 21:09:03 +0700

    Удалил Phi (оказалась ненужной)

--- problem_2/Interfaces/IBasis.cs
+++ problem_2/Interfaces/IBasis.cs
@@ -4,8 +4,6 @@ public interface IBasis
 {
     int Size { get; }
 
-    double Phi(int ifunc, double point);
-
     double Psi(int ifunc, Point2D point);
 
     double DPsi(int ifunc, int ivar, Point2D point);
