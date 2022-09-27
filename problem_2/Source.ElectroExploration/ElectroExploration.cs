b37644108f3db586123809870fef52ceb43a71b0
lexangross51 <lexangross51@gmail.com>
2022-09-27 23:33:29 +0700

    Добавлены параметры для задачи электроразведки. Написан строитель.

---
+++ problem_2/Source.ElectroExploration/ElectroExploration.cs
@@ -0,0 +1,47 @@
+﻿namespace problem_2.Source.ElectroExploration;
+
+public class ElectroExplorationBuilder
+{
+    #region ElectroExploration
+    public class ElectroExploration
+    {
+        private ElectroParameters _parameters;
+        private DirectSolver _solver = default!;
+        private Matrix<double> _matrix = default!;
+        private Vector<double> _vector = default!;
+        private readonly FEMBuilder.FEM _fem;
+
+        public ElectroExploration(ElectroParameters parameters, DirectSolver solver, FEMBuilder.FEM fem) =>
+            (_parameters, _solver, _fem) = (parameters, solver, fem);
+
+
+
+    }
+    #endregion
+
+    #region ElectroExplorationBuilder
+    private ElectroParameters _parameters = default!;
+    private DirectSolver _solver = default!;
+    private FEMBuilder.FEM _fem = default!;
+
+    public ElectroExplorationBuilder SetParameters(ElectroParameters parameters)
+    {
+        _parameters = parameters;
+        return this;
+    }
+    public ElectroExplorationBuilder SetSolver(DirectSolver solver)
+    {
+        _solver = solver;
+        return this;
+    }
+    public ElectroExploration SetFEM(FEMBuilder.FEM fem)
+    {
+        _fem = fem;
+        return this;
+    }
+
+    public static implicit operator ElectroExploration(ElectroExplorationBuilder builder)
+        => new(builder._parameters, builder._solver, builder._fem);
+    #endregion
+}
+
---
+++ problem_2/Source.ElectroExploration/ElectroParameters.cs
@@ -0,0 +1,35 @@
+﻿public readonly record struct PowerSource(Point2D A, Point2D B);
+
+public readonly record struct PowerReceiver(Point2D M, Point2D N);
+
+public class ElectroParameters
+{
+    [JsonProperty("Power sources", Required = Required.Always)]
+    public PowerSource[] PowerSources { get; init; } = Array.Empty<PowerSource>();
+
+    [JsonProperty("Power receivers", Required = Required.Always)]
+    public PowerReceiver[] PowerReceivers { get; init; } = Array.Empty<PowerReceiver>();
+
+    [JsonProperty("Primary sigma", Required = Required.Always)]
+    public double[] PrimarySigma { get; init; } = Array.Empty<double>();
+
+    public static ElectroParameters ReadJson(string jsonPath)
+    {
+        try
+        {
+            if (!File.Exists(jsonPath))
+            {
+                throw new Exception("File does not exist");
+            }
+
+            using var sr = new StreamReader(jsonPath);
+            return JsonConvert.DeserializeObject<ElectroParameters>(sr.ReadToEnd()) ??
+                   throw new NullReferenceException("Fill in the parameter data correctly");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+            throw;
+        }
+    }
+}
